4c832186feaf0a784f34aa8be1c44574f55419aa
cgcqff <cgcqff@163.com>
2019-12-24 10:59:07 +0800

    替换

--- wwnb/src/domain/DataRow.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-import java.util.LinkedHashMap;
-
-public class DataRow extends LinkedHashMap<String,Object> {
-	private static final long serialVersionUID = 7445917919712339682L;
-
-}
