5a2fbae1259577440cd0dfa888044c4e142fb093
cgcqff <cgcqff@163.com>
2020-07-09 19:59:27 +0800

    新板匹配规则添加

---
+++ bancai/src/main/java/com/bancai/cg/entity/Newpanelrule_view.java
@@ -0,0 +1,114 @@
+package com.bancai.cg.entity;
+
+public class Newpanelrule_view {
+    private Integer productId;
+    private MaterialType materialTypeId;
+    private String count;
+    private String m;
+    private String n;
+    private String a;
+    private String b;
+    private String p;
+    private String condition1;
+    private String condition2;
+    private String upWidth;
+    private String orientation;
+
+
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public MaterialType getMaterialTypeId() {
+        return materialTypeId;
+    }
+
+    public void setMaterialTypeId(MaterialType materialTypeId) {
+        this.materialTypeId = materialTypeId;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public void setM(String m) {
+        this.m = m;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
+
+    public String getCondition1() {
+        return condition1;
+    }
+
+    public void setCondition1(String condition1) {
+        this.condition1 = condition1;
+    }
+
+    public String getCondition2() {
+        return condition2;
+    }
+
+    public void setCondition2(String condition2) {
+        this.condition2 = condition2;
+    }
+
+    public String getUpWidth() {
+        return upWidth;
+    }
+
+    public void setUpWidth(String upWidth) {
+        this.upWidth = upWidth;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
