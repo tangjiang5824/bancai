106dbe1b5b18141df5ed962a0870f9bfbe251692
cgcqff <cgcqff@163.com>
2020-08-07 21:29:21 +0800

    fix

--- bancai/src/main/webapp/unuseMaterial/unuseMaterial_Query_Data.js
+++ bancai/src/main/webapp/unuseMaterial/unuseMaterial_Query_Data.js
@@ -2,33 +2,9 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
     extend:'Ext.panel.Panel',
     region: 'center',
     layout:'fit',
-    title: '原材料仓库查询',
+    title: '废料仓库查询',
     initComponent: function(){
         var itemsPerPage = 50;
-        var tableName="material_info";
-        //var materialType="1";
-        //查询数据库，返回原材料类型
-        var MaterialNameList = Ext.create('Ext.data.Store',{
-            fields : [ 'materialName'],
-            proxy : {
-                type : 'ajax',
-                url : 'material/findAllBytableName.do?tableName='+tableName,
-                reader : {
-                    type : 'json',
-                    rootProperty: 'material_info',
-                },
-                fields : ['id','materialName']
-            },
-            //字段拼接
-            // listeners:{
-            //     load:function(store,records){
-            //         for(var i=0;i<records.length;i++){
-            //             records[i].set('material_name',records[i].get('materialName')+"(规格:"+records[i].get('specification')+")");
-            //         }
-            //     }
-            // },
-            autoLoad : true
-        });
 
         //原材料类型，下拉框显示
         var MaterialTypeList = Ext.create('Ext.form.ComboBox',{
@@ -81,41 +57,22 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
                 //     value:"",
                 // },
                 {
-                    xtype:'tbtext',
-                    text:'库存数量:',
-                    margin : '0 10 0 20',
-                    itemId:'move_left',
+                    xtype: 'textfield',
+                    margin : '0 10 0 0',
+                    // fieldLabel: '',
+                    id :'wasteName',
+                    width: 100,
+                    // labelWidth: 60,
+                    name: 'wasteName',
+                    value:"",
                 },
                 {
                     xtype: 'textfield',
                     margin : '0 10 0 0',
                     // fieldLabel: '',
-                    id :'minCount',
-                    width: 100,
-                    // labelWidth: 60,
-                    name: 'minCount',
-                    value:"",
-                },{
-                    xtype:'tbtext',
-                    text:'---',
-                },
-                {
-                    xtype: 'textfield',
-                    margin : '0 10 0 0',
-                    id :'maxCount',
-                    width: 100,
-                    // labelWidth: 60,
-                    name: 'maxCount',
-                    value:"",
-                },
-
-                {
-                    xtype: 'textfield',
-                    margin : '0 10 0 40',
-                    fieldLabel: '仓库名称',
                     id :'warehouseName',
-                    width: 160,
-                    labelWidth: 60,
+                    width: 100,
+                    // labelWidth: 60,
                     name: 'warehouseName',
                     value:"",
                 },
@@ -127,17 +84,9 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
                     margin: '0 0 0 15',
                     layout: 'right',
                     handler: function(){
-                        material_Query_Data_Store.load({
+                        unusual_Query_Data_Store.load({
                             params : {
-                                //proNum : Ext.getCmp('proNum').getValue(),
-                                // startWidth : Ext.getCmp('startWidth').getValue(),
-                                // endTWidth : Ext.getCmp('endWidth').getValue(),
-                                // materialName:Ext.getCmp('materialName').getValue(),//获取id  Ext.getCmp('materialName').rawValue
-                                // materialName:Ext.getCmp('materialName').rawValue,//获取显示字段
-                                materialName:Ext.getCmp('materialName').rawValue,//获取显示字段
-                                // specification:Ext.getCmp('specification').getValue(),
-                                totalCount_min:Ext.getCmp('minCount').getValue(),
-                                totalCount_max:Ext.getCmp('maxCount').getValue(),
+                                wasteName:Ext.getCmp('wasteName').getValue(),
                                 warehouseName:Ext.getCmp('warehouseName').getValue(),
                             }
                         });
@@ -173,8 +122,8 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
                 //     }
                 // }]
         });
-        var material_Query_Data_Store = Ext.create('Ext.data.Store',{
-            id: 'material_Query_Data_Store',
+        var unusual_Query_Data_Store = Ext.create('Ext.data.Store',{
+            id: 'unusual_Query_Data_Store',
             autoLoad: true,  //初始自动加载
             fields: [],
             pageSize: itemsPerPage, // items per page,每页显示的记录条数
@@ -187,21 +136,17 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
                     totalProperty: 'totalCount'
                 },
                 params:{
-                    start: 0,
-                    limit: itemsPerPage
                 }
             },
             listeners : {
                 beforeload : function(store, operation, eOpts) {
                     store.getProxy().setExtraParams({
-                        tableName :"material_store_view",
+                        tableName :"waste_store",
                         // startWidth:Ext.getCmp('startWidth').getValue(),
                         // endWidth:Ext.getCmp('endWidth').getValue(),
 
-                        materialName:Ext.getCmp('materialName').rawValue,
+                        wasteName:Ext.getCmp('wasteName').getValue(),
                         // specification:Ext.getCmp('specification').getValue(),
-                        totalCount_min:Ext.getCmp('minCount').getValue(),
-                        totalCount_max:Ext.getCmp('maxCount').getValue(),
                         warehouseName:Ext.getCmp('warehouseName').getValue(),
                         //materialType:materialType
 
@@ -216,49 +161,27 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
 
         var grid = Ext.create('Ext.grid.Panel',{
             id: 'material_Query_Data_Main',
-            title: "原材料仓库信息记录",
-            store: material_Query_Data_Store,
+            title: "废料仓库信息记录",
+            store: unusual_Query_Data_Store,
             viewConfig : {
                 enableTextSelection : true,
                 editable:true
             },
             columns : [
                 {
-                    dataIndex : 'materialName',
-                    name : '品号',
-                    text : '品号',
+                    dataIndex : 'wasteName',
+                    name : '废料名',
+                    text : '废料名',
                     //width : 110,
                     flex :1,
-
                 },
                 {
-                    dataIndex : 'typeId',
-                    text : '原材料类型',
+                    dataIndex : 'inventoryUnit',
+                    text : '库存单位',
                     flex :.6,
                 },
-                {dataIndex : 'aValue', text : 'a值', flex :.6},
-                {dataIndex : 'bValue', text : 'b值', flex :.6},
-                {dataIndex : 'mValue', text : 'm值', flex :.6},
-                {dataIndex : 'nValue', text : 'n值', flex :.6},
-                {dataIndex : 'pValue', text : 'p值', flex :.6},
-                {dataIndex : 'orientation', text : '方向', flex :.6},
-                {dataIndex : 'inventoryUnit', text : '库存单位', flex :.6},
-                {dataIndex : 'description', text : '备注', flex :1},
                 {
-                    dataIndex : 'unitWeight',
-                    name : '单重',
-                    text : '单重',
-                    //width : 160,
-                    flex :1,
-
-                },{
-                    dataIndex : 'totalWeight',
-                    name : '总重',
-                    text : '总重',
-                    flex :1,
-                },
-                {
-                    dataIndex : 'totalCount',
+                    dataIndex : 'countStore',
                     name : '数量',
                     text : '数量',
                     flex :1,
@@ -278,34 +201,34 @@ Ext.define('unuseMaterial.unuseMaterial_Query_Data',{
             // tbar: toobar,
             dockedItems: [{
                 xtype: 'pagingtoolbar',
-                store: material_Query_Data_Store,   // same store GridPanel is using
+                store: unusual_Query_Data_Store,   // same store GridPanel is using
                 dock: 'bottom',
                 displayInfo: true,
                 displayMsg:'显示{0}-{1}条，共{2}条',
                 emptyMsg:'无数据'
-            }],
-            listeners: {
-                validateedit : function(editor, e) {
-                    var field=e.field
-                    var id=e.record.data.id
-                    if(id)
-                    Ext.Ajax.request({
-                        url:"data/EditCellById.do",  //EditDataById.do
-                        params:{
-                            tableName:tableName,
-                            field:field,
-                            value:e.value,
-                            id:id
-                        },
-                        success:function (response) {
-                            //console.log(response.responseText);
-                        }
-                    })
-                    // console.log("value is "+e.value);
-                    // console.log(e.record.data["id"]);
-
-                }
-            }
+            }]
+            // listeners: {
+            //     validateedit : function(editor, e) {
+            //         var field=e.field
+            //         var id=e.record.data.id
+            //         if(id)
+            //         Ext.Ajax.request({
+            //             url:"data/EditCellById.do",  //EditDataById.do
+            //             params:{
+            //                 tableName:tableName,
+            //                 field:field,
+            //                 value:e.value,
+            //                 id:id
+            //             },
+            //             success:function (response) {
+            //                 //console.log(response.responseText);
+            //             }
+            //         })
+            //         // console.log("value is "+e.value);
+            //         // console.log(e.record.data["id"]);
+            //
+            //     }
+            // }
         });
 
         // this.tbar = toobar;
